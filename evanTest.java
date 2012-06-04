17c0f6da10ea3c96412ff6858c8841fe85519548
darthnad3r <Evan@10.20.1.246>
2012-06-04 15:28:01 -0700

    Grid added

--- evanTest.java
+++
@@ -1,8 +0,0 @@
-
-public class evanTest {
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Let's see if this works.");
-	}
-}
--- src/sequencerFrame.java
+++ src/sequencerFrame.java
@@ -3,169 +3,163 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 
 public class sequencerFrame
 {
 
-	private JFrame frame;
+    private JFrame frame;
+    private static int[] mousePos;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args)
-	{
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				try
-				{
-					sequencerFrame window = new sequencerFrame();
-					window.frame.setVisible(true);
-				} catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    sequencerFrame window = new sequencerFrame();
+                    window.frame.setVisible(true);
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public sequencerFrame()
-	{
-		initialize();
-	}
+    /**
+     * Create the application.
+     */
+    public sequencerFrame()
+    {
+        initialize();
+        mousePos = new int[2];
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize()
-	{
-		frame = new JFrame();
-		frame.getContentPane().addMouseListener(new MouseAdapter() 
-		{
-			@Override
-			public void mouseClicked(MouseEvent e) 
-			{
-				//System.out.println("Click at "+ MouseInfo.getPointerInfo().getLocation());
-			}
-		});
-		frame.setBounds(100, 100, 535, 510);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		final Grid panel = new Grid(8,8);
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setBounds(100,40,300,200);
-		panel.setBackground(new java.awt.Color(255,255,255));
-		
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize()
+    {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 535, 510);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final Grid panel = new Grid(8,8);
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        panel.setBounds(100,40,300,200);
+        panel.setBackground(new java.awt.Color(255,255,255));
 
-	}
-	private class GPanel extends JPanel
-	{
-		public void paintComponent(Graphics g)
-		{
+        frame.getContentPane().addMouseListener(new MouseAdapter()
+        {
+            @Override
+            public void mouseClicked(MouseEvent e)
+            {
+                PointerInfo pi = MouseInfo.getPointerInfo();
+                Point point = new Point(pi.getLocation());
+                SwingUtilities.convertPointFromScreen(point, e.getComponent());
+                System.out.println("("+(int)point.getX()+","+(int)point.getY()+")");
+                mousePos[0] = (int)point.getX();
+                mousePos[1] = (int)point.getY();
+                panel.repaint();
 
-			super.paintComponent(g);
+            }
+        });
+    }
 
-			/*g.setColor(Color.black);
-			g.drawRect(0, 0, 50, 50);
-			g.drawRect(0, 50, 50, 50);
-			g.drawRect(0, 100, 50, 50);
-			g.drawRect(0, 150, 50, 50);
-			g.drawRect(0, 200, 50, 50);
-			g.drawRect(0, 250, 50, 50);
-			g.drawRect(0, 300, 50, 50);
-			g.drawRect(0, 350, 50, 50);
-			g.drawRect(0, 400, 50, 50);
+    private class Grid extends JPanel
+    {
+        int row;
+        int col;
+        int iBoundMax;
+        int iBoundMin;
+        int kBoundMax;
+        int kBoundMin;
+        boolean[][] doa={
+                {false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false}
+        };
+        Grid(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            for(int i=0;i<col;i++)
+            {
+                for(int k=0;k<row;k++)
+                {
+                    g.drawRect(((i*50)+25),k*50,50,50);
+                    iBoundMin = ((i*50)+25);
+                    iBoundMax = ((i*50)+25)+50;
+                    kBoundMin = (k*50);
+                    kBoundMax = (k*50)+50;
 
-			g.drawRect(50, 0, 50, 50);
-			g.drawRect(50, 50, 50, 50);
-			g.drawRect(50, 100, 50, 50);
-			g.drawRect(50, 150, 50, 50);
-			g.drawRect(50, 200, 50, 50);
-			g.drawRect(50, 250, 50, 50);
-			g.drawRect(50, 300, 50, 50);
-			g.drawRect(50, 350, 50, 50);
-			g.drawRect(50, 400, 50, 50);
+                    if(isInBounds(iBoundMin, iBoundMax, kBoundMin, kBoundMax))
+                    {
+                        if(doa[i][k] ==false)
+                        {
+                            doa[i][k] = true;
+                        }
+                        else
+                        {
+                            doa[i][k] = false;
+                        }
 
-			g.drawRect(100, 0, 50, 50);
-			g.drawRect(100, 50, 50, 50);
-			g.drawRect(100, 100, 50, 50);
-			g.drawRect(100, 150, 50, 50);
-			g.drawRect(100, 200, 50, 50);
-			g.drawRect(100, 250, 50, 50);
-			g.drawRect(100, 300, 50, 50);
-			g.drawRect(100, 350, 50, 50);
-			g.drawRect(100, 400, 50, 50);
+                    }
+                    if(doa[i][k]){
+                        g.fillRect(i*50+25, k*50, 50, 50);
+                    }
 
-			g.drawRect(150, 0, 50, 50);
-			g.drawRect(150, 50, 50, 50);
-			g.drawRect(150, 100, 50, 50);
-			g.drawRect(150, 150, 50, 50);
-			g.drawRect(150, 200, 50, 50);
-			g.drawRect(150, 250, 50, 50);
-			g.drawRect(150, 300, 50, 50);
-			g.drawRect(150, 350, 50, 50);
-			g.drawRect(150, 400, 50, 50);
+                }
+            }
+            g.drawString("C", 11, 380);
+            g.drawString("D", 11, 330);
+            g.drawString("E", 11, 280);
+            g.drawString("F", 11, 230);
+            g.drawString("G", 11, 180);
+            g.drawString("A", 11, 130);
+            g.drawString("B", 11, 80);
+            g.drawString("C", 11, 30);
 
-			g.drawRect(200, 0, 50, 50);
-			g.drawRect(200, 50, 50, 50);
-			g.drawRect(200, 100, 50, 50);
-			g.drawRect(200, 150, 50, 50);
-			g.drawRect(200, 200, 50, 50);
-			g.drawRect(200, 250, 50, 50);
-			g.drawRect(200, 300, 50, 50);
-			g.drawRect(200, 350, 50, 50);
-			g.drawRect(200, 400, 50, 50);
+        }
 
-			g.drawRect(250, 0, 50, 50);
-			g.drawRect(250, 50, 50, 50);
-			g.drawRect(250, 100, 50, 50);
-			g.drawRect(250, 150, 50, 50);
-			g.drawRect(250, 200, 50, 50);
-			g.drawRect(250, 250, 50, 50);
-			g.drawRect(250, 300, 50, 50);
-			g.drawRect(250, 350, 50, 50);
-			g.drawRect(250, 400, 50, 50);
 
-			g.drawRect(300, 0, 50, 50);
-			g.drawRect(300, 50, 50, 50);
-			g.drawRect(300, 100, 50, 50);
-			g.drawRect(300, 150, 50, 50);
-			g.drawRect(300, 200, 50, 50);
-			g.drawRect(300, 250, 50, 50);
-			g.drawRect(300, 300, 50, 50);
-			g.drawRect(300, 350, 50, 50);
-			g.drawRect(300, 400, 50, 50);
+    }
+    private static boolean isInBounds(int xMin, int xMax, int yMin, int yMax)
+    {
 
-			g.drawRect(350, 0, 50, 50);
-			g.drawRect(350, 50, 50, 50);
-			g.drawRect(350, 100, 50, 50);
-			g.drawRect(350, 150, 50, 50);
-			g.drawRect(350, 200, 50, 50);
-			g.drawRect(350, 250, 50, 50);
-			g.drawRect(350, 300, 50, 50);
-			g.drawRect(350, 350, 50, 50);
-			g.drawRect(350, 400, 50, 50);
 
-			g.drawRect(400, 0, 50, 50);
-			g.drawRect(400, 50, 50, 50);
-			g.drawRect(400, 100, 50, 50);
-			g.drawRect(400, 150, 50, 50);
-			g.drawRect(400, 200, 50, 50);
-			g.drawRect(400, 250, 50, 50);
-			g.drawRect(400, 300, 50, 50);
-			g.drawRect(400, 350, 50, 50);
-			g.drawRect(400, 400, 50, 50);*/
-		}
-	}
-	
-	
+        if(mousePos[0] < xMax && mousePos[0] > xMin && mousePos[1] < yMax && mousePos[1] > yMin )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
+
+
